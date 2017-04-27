f3c24bc06885ce81358f20e5b7dee9b390aaf22b
Stanley Mwangi <stanleym2711@gmail.com>
2017-04-27 02:32:23 +0100

    Basic exploration of SP500 data

--- week_1_intro_to_quantitative_investing/read_csv_data.R
+++ week_1_intro_to_quantitative_investing/read_csv_data.R
@@ -1,2 +1,16 @@
 # First R script to read yahoo finance S&P finance data and assign to a variable
-SP500<-read.csv(file="/week_1_intro_to_quantitative_investing/SP500_data.csv")
+
+# SP500 monthly data from Jan 1990 to Dec 2015  
+SP500<-read.csv(file="./week_1_intro_to_quantitative_investing/SP500_data.csv")
+
+# Retrieve column names
+colnames(SP500)
+
+# Retrieve closing price by indexing by column name
+price<-SP500$Close
+
+# Retrieve volume by indexing by column number
+volume<-SP500[,6]
+
+# Retrieve opening price for July 2011 using row name followed by column name
+july_open<-SP500[54,2]
